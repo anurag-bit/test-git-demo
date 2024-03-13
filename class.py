1b1cf50bf8dbaef9eaba5e3da6c5584ce6c2869c
Anurag Kumar Singh <singh.ask@outlook.com>
2024-03-14 00:47:07 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def print_info(self):
+        print("Student Information:")
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Grade:", self.grade)
+
+
+name = input("Enter student name: ")
+age = input("Enter student age: ")
+grade = input("Enter student grade: ")
+
+student = Student(name, age, grade)
+
+student.print_info()
